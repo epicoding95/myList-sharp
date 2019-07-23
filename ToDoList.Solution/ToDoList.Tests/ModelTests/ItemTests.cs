884620c8cbcac472c3b3091a97d15efb899dfbfe
Student <example@example.com>
2019-07-23 16:16:49 -0700

    fully works

--- ToDoList.Solution/ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Solution/ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,13 +1,80 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
+using System.Collections.Generic;
+using System;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTest
+  ////IDisposable is what is reffered to as an interface
+  //  : syntax extends functionally from IDisposable  TO our ItemTest class
+  public class ItemTest: IDisposable
   {
 
-    // Test methods will go here.
+    public void Dispose()
+    {
+     Item.ClearAll();
+    } 
+
+    [TestMethod]
+    ////ItemConstructor-- is the method were testing
+    ////CreatesInstanceOf--describes the behavior we want in our method
+    ////item is the expected return value of the method were testing
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test");
+      ////typeof-- returns the data type of a class
+      ////gettype() returns the data type of a specific object
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      ////create a new string called description
+      ///create a new item object, passsing the description
+      ///Record the result of retrieving the description property of newitem
+      /////Last line will confirm the retrieved item object matches the description
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> {newItem1, newItem2};
+
+      List<Item> result = Item.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemsList()
+    {
+      ///Arrange
+      List<Item> newList = new List<Item> { };
+
+      //Act
+      List<Item> result = Item.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+
 
   }
 }
--- ToDoList.Solution/ToDoList/Models/Item.cs
+++ ToDoList.Solution/ToDoList/Models/Item.cs
@@ -1,10 +1,36 @@
-namespace ToDoList.Models
+using System;
+using System.Collections.Generic;
+
+
+namespace ToDoList
 {
   public class Item
   {
+    public string Description { get; set; }
+    private static List<Item> _instances = new List<Item> {};
 
-    // properties, methods, etc. will go here.
+    public Item(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
+
+    public static List<Item> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      // _instances.Clear();
+    }
 
 
   }
 }
+
+// Item itemInstance = new Item(){}
+// itemInstance._instances
+
+
+// Item._instances;
+// Item.GetAll()
