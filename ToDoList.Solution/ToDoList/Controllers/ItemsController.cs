82ff4ea06696c241f16458074427056e576ba3db
Student <example@example.com>
2019-07-30 16:43:15 -0700

    added functionality

--- ToDoList.Solution/ToDoList/Controllers/ItemsController.cs
+++ ToDoList.Solution/ToDoList/Controllers/ItemsController.cs
@@ -4,33 +4,33 @@ using System.Collections.Generic;
 
 namespace ToDoList.Controllers
 {
-  public class ItemsController : Controller
-  {
-
-    [HttpGet("/items")]
-    public ActionResult Index()
+    public class ItemsController : Controller
     {
-    List<Item> allItems = Item.GetAll();
-      return View(allItems);
+        [HttpGet("/items")]
+        public ActionResult Index()
+        {
+            List<Item> allItems = Item.GetAll();
+            return View(allItems);
+        }
+
+        [HttpGet("/items/new")]
+        public ActionResult CreateForm()
+        {
+            return View();
+        }
+
+        [HttpPost("/items")]
+        public ActionResult Create(string description)
+        {
+            Item myItem = new Item(description);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost("/items/delete")]
+        public ActionResult DeleteAll()
+        {
+            Item.ClearAll();
+            return View();
+        }
     }
-
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm()
-    {
-      return View();
-    }
-
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-
-
-      /// we are telling the view() method to return the "index" view. we have to do this because we are no longer routing a view wth the same exact name as our route method.
-
-      // the second argument specifies what the model property on the view should be
-    {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
-    }
-
-  }
 }
